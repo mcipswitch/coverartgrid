404f290db29105d94ecd289143e4006c976c22e5
Priscilla Ip <priscillaip@Priscillas-MacBook-Pro-2.local>
2020-08-09 15:17:26 -0400

    Add swiftui project

---
+++ CoverArtGrid/CoverArtGrid/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CoverArtGrid
+//
+//  Created by Priscilla Ip on 2020-08-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoverArtGrid/CoverArtGrid/CoverArtGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverArtGridApp.swift
+//  CoverArtGrid
+//
+//  Created by Priscilla Ip on 2020-08-09.
+//
+
+import SwiftUI
+
+@main
+struct CoverArtGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
